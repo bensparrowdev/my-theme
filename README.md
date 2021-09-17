6f4bade1d83f37362316d091bc24dbcd823ab9ca
Ben <bensparrowdev@gmail.com>
2021-09-17 13:08:34 +0100

    first commit, adding files

---
+++ README.md
@@ -0,0 +1,1 @@
+# WordPress Theme - my-theme
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package myTheme
+ */
+
+
+ ?>
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wordpress theme</title>
+</head>
+<body>
+ Hello WordPress
+</body>
+</html>
